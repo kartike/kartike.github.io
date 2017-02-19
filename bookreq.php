ca71ff6df34a0cd87f4956a3758b6d0b356a91f4
Kartike Sachdeva <kartike.sachdeva@learner.manipal.edu>
2017-02-20 00:43:06 +0530

    modi

--- bookreq.php
+++ bookreq.php
@@ -6,13 +6,13 @@ $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
 $name = $_POST['name'];
 $email = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-$msg = "Name: " . $name . "\nPhone: " . $phone . "\nEmail: " . $email . "\nMessage: " . $message;
+$author = $_POST['author'];
+$book = $_POST['book'];
+$msg = "Name: " . $name . "\nEmail: " . $email ."\nAuthor: " . $author .  "\nBook details: " . $book;
 $msg = wordwrap($msg,70);
 $to = "sachdeva.kartike@gmail.com";
-$subject = "Contact Submission";
+$subject = "Book Request";
 
 mail($to, $subject, $msg, $headers);
-echo 'Thank you for your submission.  Please <a href="/index.html">Click here to return to our homepage.';
+echo 'Thank you for your request. We will get back to you soon.  Please <a href="/index.html">Click here to return to our homepage.';
 ?>
